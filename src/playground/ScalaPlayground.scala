cbc17d2a4951a5485f06e2cdac51143b576a00ad
Sangkon Han <sigmadream@gmail.com>
2022-02-16 01:13:19 +0900

    러닝 스칼라 5장 연습문제

--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -3,9 +3,18 @@ package playground
 import scala.annotation.tailrec
 
 object ScalaPlayground extends App {
-  def stringify[A, B, C](t: (A, B, C)): (A, String, B, String, C, String) = {
-    (t._1, t._1.toString, t._2, t._2.toString, t._3, t._3.toString)
+  def conditional[A](x: A, p: A => Boolean, f: A => String): String = {
+    if (p(x)) f(x) else ""
   }
 
-  println(stringify(1, 1.1, 1L))
+  def typeSafely(i: Int): String = {
+    val a1 = conditional[Int](i, _ % 3 == 0, _ => "type")
+    val a2 = conditional[Int](i, _ % 5 == 0, _ => "safe")
+    val a3 = conditional[Int](i, _ % 3 > 0 && i % 5 > 0, x => s"$x")
+    a1 + a2 + a3
+  }
+
+  val sequence = 1 to 100 map typeSafely
+
+  println(sequence.mkString("\n"))
 }
