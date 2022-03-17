fac9d0ef6da780c723872f1f9845b0514e4f4d8b
Sangkon Han <sigmadream@gmail.com>
2022-03-17 18:33:59 +0900

    // wip

--- src/test/scala/exercises/learningscala/ch09/HtmlUtilsSpec.scala
+++ src/test/scala/exercises/learningscala/ch09/HtmlUtilsSpec.scala
@@ -1,19 +1,21 @@
 package exercises.learningscala.ch09
 
+import org.scalatest.flatspec.AnyFlatSpec
 
-//class HtmlUtilsSpec extends FlatSpec with Matchers {
-//
-//  "The Html Utils object" should "remove single elements" in {
-//    HtmlUtils.removeMarkup("<br/>") should equal("")
-//  }
-//
-//  it should "remove paired elements" in {
-//    HtmlUtils.removeMarkup("<b>Hi</b>") should equal("Hi")
-//  }
-//
-//  it should "have no effect on empty strings" in {
-//    val empty = true
-//    HtmlUtils.removeMarkup("").isEmpty should be(empty)
-//  }
-//
-//}
+
+class HtmlUtilsSpec extends AnyFlatSpec {
+
+  "The Html Utils object" should "remove single elements" in {
+    HtmlUtils.removeMarkup("<br/>").equals("")
+  }
+
+  it should "remove paired elements" in {
+    HtmlUtils.removeMarkup("<b>Hi</b>").equals("Hi")
+  }
+
+  it should "have no effect on empty strings" in {
+    val empty = true
+    HtmlUtils.removeMarkup("").isEmpty.equals(empty)
+  }
+
+}
